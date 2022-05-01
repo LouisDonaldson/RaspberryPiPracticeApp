1400add227fc9e7c17dbb35eb7fb3ee2702f2b4e
SittingBanana01 <louis.donaldson42@gmail.com>
2022-05-01 15:19:53 +0100

    added release test

--- RaspberryPiPracticeApp/RaspberryPiPracticeApp/Source.cpp
+++ RaspberryPiPracticeApp/RaspberryPiPracticeApp/Source.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	for (int i = 0; i < argc; i++) {
+	std::cout << "Command Line Arguments inputted: " << std::endl;
+	for (int i = 1; i < argc; i++) {
 		std::cout << argv[i] << " ";
 	}
 	std::cout << std::endl;
